a8b78d3b48a8392f2925c2ea9410d12fdb79cc01
PavanPochi06 <pavanpochi06@gmail.com>
2026-03-31 12:00:27 +0530

    implement AllRooms page and add route to App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Hero from "./pages/Home";
 import Footer from "./components/Footer";
+import AllRooms from "./pages/AllRooms";
 
 const App = () => {
 
@@ -12,6 +13,7 @@ const App = () => {
       <div className="min-h-[70vh]">
         <Routes>
           <Route path="/" element={<Hero />} />
+          <Route path="/all-rooms" element={<AllRooms />} />
         </Routes>
       </div>
       <Footer />
