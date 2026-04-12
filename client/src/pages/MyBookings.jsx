14b08bc7ba621670079dd33426aa3910db0f48c6
PavanPochi06 <pavanpochi06@gmail.com>
2026-04-12 14:41:51 +0530

    implement hotel registration component with routing integration

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -80,7 +80,7 @@ const MyBookings = () => {
                   </div>
                   {!booking.isPaid && (
                     <button className="px-4 py-1.5 mt-4 text-xs border border-gray-400 rounded-full hover:bg-gray-50 transition-all cursor-pointer">
-                      Pay Now
+                      Pay Now 
                     </button>
                   )}
                 </div>
